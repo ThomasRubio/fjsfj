9c1724a0041b68866f08226de1728b88a2278534
Mariusbre <breinlenmarius24@gmail.com>
2023-06-13 18:28:27 +0200

    cc

--- filmographie/filmographie/appfilmo/models.py
+++ filmographie/filmographie/appfilmo/models.py
@@ -30,7 +30,7 @@ class Categorie(models.Model):
 class Acteurs(models.Model):
     nom = models.CharField(max_length=100, blank=False)
     prenom = models.CharField(max_length=100, blank=False)
-    age = models.IntegerField(blank=False)
+    age = models.IntegerField(validators=[MinValueValidator(0)],blank=False)
 
     def __str__(self):
         return self.nom
